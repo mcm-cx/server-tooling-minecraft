c515cac8a1cc9b4d06bf11a5294601e0ea355d17
Michael J. Stallinger <mj@stallinger.dev>
2025-04-10 13:54:04 +0000

    Clean up user config scripts, rename scripts

--- setup/user-create.sh
+++ setup/user-create.sh
@@ -18,3 +18,8 @@ sudo useradd \
   --shell /usr/sbin/nologin \
   --comment "Service User for Backups" \
   svc-backup
+
+# change home dir for service users
+sudo usermod -d /opt/minecraft/minecraft-hotel svc-minecraft-hotel
+sudo usermod -d /opt/minecraft/minecraft-frostbite svc-minecraft-frostbite
+sudo usermod -d /opt/backup svc-backup
--- setup/usermod.sh
+++
@@ -1,4 +0,0 @@
-sudo usermod -d /opt/minecraft/minecraft-hotel svc-minecraft-hotel
-sudo usermod -d /opt/minecraft/minecraft-frostbite svc-minecraft-frostbite
-sudo usermod -d /opt/backup svc-backup
-

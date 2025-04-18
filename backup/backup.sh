e9fed13f7c9106d3a19105c25caa746a1f88e821
Michael J. Stallinger <mj@stallinger.dev>
2025-04-18 00:24:08 +0000

    Add first release of backup-script Hard coded hotel and frostbite-world backups

--- backup/backup.sh
+++ backup/backup.sh
@@ -7,7 +7,7 @@ BACKUP_SOURCE='/opt/minecraft'
 BACKUP_DIR='/opt/backup/minecraft'
 LOG_DIR='/opt/backup/logs'
 LOGFILE="$LOG_DIR/minecraft-$TIMESTAMP.log"
-RCLONE_REMOTE='onedrive-backup-mcm-cx:minecraft'
+RCLONE_REMOTE='onedrive-backup-mcm-cx:minecraft/jaedenar.mcm.cx'
 
 # ToDo: array
 HOTEL_PATH='/minecraft-hotel'
@@ -30,8 +30,8 @@ sleep 10
 
 #### BACKUP HOTEL ####
 echo "[*] Backing up world: TheHotel"
-HOTEL_ZIP="$BACKUP_DIR/hotel-$TIMESTAMP.zip"
-zip -r "$HOTEL_ZIP" "$BACKUP_SOURCE/$HOTEL_PATH/worlds"
+HOTEL_ZIP="$BACKUP_DIR/hotel-$TIMESTAMP.tar.gz"
+tar -czf "$HOTEL_ZIP" -C "$BACKUP_SOURCE$HOTEL_PATH/worlds" .
 
 #echo "[*] Sammle JSON + Properties: hotel"
 #HOTEL_META_DIR="$BACKUP_DIR/hotel-meta-$TIMESTAMP"
@@ -41,8 +41,8 @@ zip -r "$HOTEL_ZIP" "$BACKUP_SOURCE/$HOTEL_PATH/worlds"
 
 #### BACKUP FROSTBITE ####
 echo "[*] Backing up world: frostbite"
-FROSTBITE_ZIP="$BACKUP_DIR/frostbite-$TIMESTAMP.zip"
-zip -r "$FROSTBITE_ZIP" "$BACKUP_SOURCE/$FROSTBITE_PATH/worlds"
+FROSTBITE_ZIP="$BACKUP_DIR/frostbite-$TIMESTAMP.tar.gz"
+tar -czf "$FROSTBITE_ZIP" -C "$BACKUP_SOURCE$FROSTBITE_PATH/worlds" .
 
 #echo "[*] Sammle JSON + Properties: frostbite"
 #FROSTBITE_META_DIR="$BACKUP_DIR/frostbite-meta-$TIMESTAMP"
@@ -51,23 +51,20 @@ zip -r "$FROSTBITE_ZIP" "$BACKUP_SOURCE/$FROSTBITE_PATH/worlds"
 #cp "$FROSTBITE_PATH/server.properties" "$FROSTBITE_META_DIR" || true
 
 
+echo "[*] Starting servers..."
+sudo /usr/bin/systemctl start minecraft-hotel
+sudo /usr/bin/systemctl start minecraft-frostbite
+
 
 echo "[*] Uploading ..."
 
 # hotel
-rclone copy "$HOTEL_ZIP" "$RCLONE_REMOTE/hotel/" --progress
-rclone copy "$BACKUP_SOURCE/$HOTEL_PATH/*.json" "$RCLONE_REMOTE/hotel/" --progress
-rclone copy "$BACKUP_SOURCE/$HOTEL_PATH/*.properties" "$RCLONE_REMOTE/hotel/" --progress
+rclone copy "$HOTEL_ZIP" "$RCLONE_REMOTE/hotel/"
+rclone copy "$BACKUP_SOURCE/$HOTEL_PATH" "$RCLONE_REMOTE/hotel/" --include "*.json" --include "*.properties" --max-depth 1
 
 # frostbite
-rclone copy "$FROSTBITE_ZIP" "$RCLONE_REMOTE/frostbite/" --progress
-rclone copy "$BACKUP_SOURCE/$FROSTBITE_PATH/*.json" "$RCLONE_REMOTE/frostbite/" --progress
-rclone copy "$BACKUP_SOURCE/$FROSTBITE_PATH/*.properties" "$RCLONE_REMOTE/frostbite/" --progress
-
-
-echo "[*] Starting servers..."
-sudo /usr/bin/systemctl start minecraft-hotel
-sudo /usr/bin/systemctl start minecraft-frostbite
+rclone copy "$FROSTBITE_ZIP" "$RCLONE_REMOTE/frostbite/"
+rclone copy "$BACKUP_SOURCE/$FROSTBITE_PATH" "$RCLONE_REMOTE/frostbite/" --include "*.json" --include "*.properties" --max-depth 1
 
 #### clean up ####
 #rm -rf "$HOTEL_META_DIR" "$FROSTBITE_META_DIR"
